840627fe48509e4b4405fa4605d3fd3a12b72d38
githubdagi username <githubga kiritilgan email>
2026-09-15 20:14:32 +0500

    first commit

---
+++ dscript.js
@@ -0,0 +1,17 @@
+let like = document.querySelectorAll(".count")
+let buy = document.querySelectorAll(".hard")
+let btn = document.querySelectorAll(".btn")
+const cart_badge = document.querySelectorAll(".cart-badge")
+
+
+
+
+
+
+
+
+
+
+
+
+
