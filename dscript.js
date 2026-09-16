aa3cb507cd0c34005f38a4c3e0bb8b691a839273
githubdagi username <githubga kiritilgan email>
2026-09-16 12:55:41 +0500

    ooouueees. My js count started working

--- dscript.js
+++ dscript.js
@@ -1,8 +1,20 @@
 let like = document.querySelectorAll(".count")
 let buy = document.querySelectorAll(".hard")
 let btn = document.querySelectorAll(".btn")
-const cart_badge = document.querySelectorAll(".cart-badge")
+let card_id = document.getElementById("id")
+const cart_badge = document.querySelector(".cart-badge")
 
+
+let count = 0;
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", (e) =>{
+        const id = e.target.getAttribute("data-id")
+        count++;
+        cart_badge.textContent = count
+    })
+    
+}
 
 
 
